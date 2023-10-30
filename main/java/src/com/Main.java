9469d885479d3fa02a40fb89ec2ab85895b84b8b
dci-student <Terissa>
2023-10-30 11:19:50 +0100

    Inventory Class

--- main/java/src/com/Main.java
+++ main/java/src/com/Main.java
@@ -1,13 +1,35 @@
 package com;
 
 import com.bookstore.Book;
+import com.bookstore.Inventory;
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    Book myBook = new Book("Harry Potter", "J.K. Rowling", 39.99, 10);
-    System.out.println(myBook.toString());
+    /*Book myBook = new Book("Harry Potter", "J.K. Rowling", 39.99, 10);
+    System.out.println(myBook.toString());*/
 
+    // Create an instance of the Inventory class with a capacity of 4 books
+    Inventory myInventory = new Inventory(4);
+
+    // Add some books to the inventory
+    myInventory.addBook(new Book("Harry Potter", "J. K. Rowling", 39.99 , 10));
+    myInventory.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 20.99 , 8));
+    myInventory.addBook(new Book("Limbe To Lagos", "Emmanuel Iduma", 10.99 , 5));
+    myInventory.addBook(new Book("The Latte Factor", "David Bach", 30.99 , 20));
+
+    // inventory after current inventory
+    System.out.println(" Inventory after adding books:");
+    myInventory.displayInventory();
+
+    // Remove a book from the inventory
+    myInventory.removeBook("The Hobbit");
+
+    // Display the inventory again
+    System.out.println(" Inventory after removing a book:");
+    myInventory.displayInventory();
   }
+
 }
--- main/java/src/com/bookstore/Inventory.java
+++ main/java/src/com/bookstore/Inventory.java
@@ -1,4 +1,58 @@
 package com.bookstore;
 
+
 public class Inventory {
+    private Book[] books; //declare a private 1D array of Book objects.
+    private int currentIndex;  // represents the current number of books in the inventory.
+
+    // Constructor to initialize the array and other variables
+    public Inventory ( int size) {
+        books = new Book[size];
+        currentIndex = 0;
+    }
+
+    //Method to add a book object to the array
+    public void addBook (Book book) {
+        if (currentIndex < books.length) {
+            books[currentIndex] = book;
+            currentIndex++;
+            System.out.println("Book added to the Inventory: " + book.getTitle());
+        } else {
+            System.out.println("Inventory is full. Cannot add more books");
+        }
+    }
+    // Method to remove a Book object from the array based on the title
+    public void removeBook (String title) { // It takes a String argument title, which represents the title of the book you want to remove from the inventory
+        for (int i = 0; i < currentIndex; i++) {
+            if (books[i] != null && books[i].getTitle().equals(title)) {
+                books[i] = null;
+                // Shift remaining books to fill the gab left
+                for (int j=i; j<currentIndex-1; j++) {
+                    books[j] = books[j+1]; // overwriting the book at index j with the book at index j + 1.
+                    // This effectively moves the book from the next position in the array to the current position,
+                    // shifting all remaining books one slot to the left
+                    // The loop will continue as long as j is less than currentIndex - 1. currentIndex represents the
+                    // current number of books in the inventory. The loop continues until one less than the current index to avoid going out of bounds.
+                }
+                books[currentIndex-1] = null;
+                currentIndex--;
+                System.out.println("Book removed from Inventory: " + title);
+                return; // Exit the method after removing the book. This ensures that the method doesn't continue
+                // checking the remaining elements in the array once the book has been removed.
+            }
+        }
+        System.out.println("Book not found in Inventory: " + title);
+    }
+
+    // Method to display the current inventory
+    public void displayInventory() {
+        System.out.println("Current Inventory:");
+        for(int i=0; i<currentIndex; i++) {
+            if (books[i] != null) {
+                System.out.println(books[i]);
+            }
+        }
+    }
+
+
 }
